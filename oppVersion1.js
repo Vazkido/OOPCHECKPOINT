492aba042e28e2bd5918bb43afc46cc615a49314
vazkido <vazkid02@gmail.com>
2025-02-05 14:49:09 -0500

    initial Commit

---
+++ oppVersion1.js
@@ -0,0 +1,62 @@
+// CLASS WORK TO RECALL OOP LAST CLASS
+
+// class Animal {
+//     constructor(name, age, color){
+//         this.name = name
+//         this.age = age
+//         this.color = color
+//     }
+//     greet = function (){
+//         console.log(`${this.name} is aggressive and is ${this.age} old`)
+//     }
+// };
+
+// class Dog extends Animal{
+//     constructor(){
+//         super()
+//     };
+// }
+
+// class Cat extends Animal{
+//     constructor() {
+//         super(name, age, color)
+//     }
+// };
+
+// console.log(Dog.greet)
+
+class Product {
+  constructor(id, name, price) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+  }
+}
+
+class shoppingCartItem {
+  constructor(products, quantity) {
+    this.products = products;
+    this.quantity = quantity;
+  }
+  getTotaltemPrice() {
+    return this.products.price * this.quantity;
+  }
+}
+
+class ShoppingCart {
+  constructor() {
+    this.item = [];
+  }
+  getTotalPrice() {
+    //   let tot = 0
+    //   for(let item of this.items){
+    //     tot+= item.getTotalItemsPrice()
+    //   }
+    //   return tot            #INSTEAD WE DO THIS IT THIS WAY
+    return this.items.reduce((sum, a) => sum + a.getTotaltemPrice, 0);
+  }
+
+  addItem(product, quantity) {
+    this.item.pusj(new shoppingCartItem(product, quantity));
+  }
+}
