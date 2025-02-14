ff39638bee492996e0d8237f489f4119563ce50b
vazkido <vazkid02@gmail.com>
2025-02-14 18:57:47 -0500

    oop Version1 js update

--- oppVersion1.js
+++ oppVersion1.js
@@ -1,62 +1,101 @@
-// CLASS WORK TO RECALL OOP LAST CLASS
+let products = document.querySelectorAll('.card-item')
 
-// class Animal {
-//     constructor(name, age, color){
-//         this.name = name
-//         this.age = age
-//         this.color = color
-//     }
-//     greet = function (){
-//         console.log(`${this.name} is aggressive and is ${this.age} old`)
-//     }
-// };
 
-// class Dog extends Animal{
-//     constructor(){
-//         super()
-//     };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // CLASS WORK TO RECALL OOP LAST CLASS
+
+// // class Animal {
+// //     constructor(name, age, color){
+// //         this.name = name
+// //         this.age = age
+// //         this.color = color
+// //     }
+// //     greet = function (){
+// //         console.log(`${this.name} is aggressive and is ${this.age} old`)
+// //     }
+// // };
+
+// // class Dog extends Animal{
+// //     constructor(){
+// //         super()
+// //     };
+// // }
+
+// // class Cat extends Animal{
+// //     constructor() {
+// //         super(name, age, color)
+// //     }
+// // };
+
+// // console.log(Dog.greet)
+
+// class Product {
+//   constructor(id, name, price) {
+//     this.id = id;
+//     this.name = name;
+//     this.price = price;
+//   }
 // }
 
-// class Cat extends Animal{
-//     constructor() {
-//         super(name, age, color)
-//     }
-// };
+// class shoppingCartItem {
+//   constructor(products, quantity) {
+//     this.products = products;
+//     this.quantity = quantity;
+//   }
+//   getTotaltemPrice() {
+//     return this.products.price * this.quantity;
+//   }
+// }
 
-// console.log(Dog.greet)
+// class ShoppingCart {
+//   constructor() {
+//     this.item = [];
+//   }
+//   getTotalPrice() {
+//     //   let tot = 0
+//     //   for(let item of this.items){
+//     //     tot+= item.getTotalItemsPrice()
+//     //   }
+//     //   return tot            #INSTEAD WE DO THIS IT THIS WAY
+//     return this.items.reduce((sum, a) => sum + a.getTotaltemPrice, 0);
+//   }
 
-class Product {
-  constructor(id, name, price) {
-    this.id = id;
-    this.name = name;
-    this.price = price;
-  }
-}
-
-class shoppingCartItem {
-  constructor(products, quantity) {
-    this.products = products;
-    this.quantity = quantity;
-  }
-  getTotaltemPrice() {
-    return this.products.price * this.quantity;
-  }
-}
-
-class ShoppingCart {
-  constructor() {
-    this.item = [];
-  }
-  getTotalPrice() {
-    //   let tot = 0
-    //   for(let item of this.items){
-    //     tot+= item.getTotalItemsPrice()
-    //   }
-    //   return tot            #INSTEAD WE DO THIS IT THIS WAY
-    return this.items.reduce((sum, a) => sum + a.getTotaltemPrice, 0);
-  }
-
-  addItem(product, quantity) {
-    this.item.push(new shoppingCartItem(product, quantity));
-  }
-}
+//   addItem(product, quantity) {
+//     this.item.push(new shoppingCartItem(product, quantity));
+//   }
+// }
