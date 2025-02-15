50f3de22f5e2bf621503e2caec8544e966a7659b
vazkido <vazkid02@gmail.com>
2025-02-15 17:12:51 -0500

    oop Version1 js 1.0.1

--- oppVersion1.js
+++ oppVersion1.js
@@ -1,11 +1,12 @@
 let products = document.querySelectorAll('.card-item')
 
-products.forEach(items) => {
+products.forEach(item) => {
   let plus = item.querySelector('.fa-plus-circle');
   let minus = item.querySelector('.fa-minus-circle');
+  let delBtn = item.querySelector('.fa-trash-alt');
+  let heart = item.querySelector('.fa-heart');
+  let qty = 
 }
-
-
 
 
 
